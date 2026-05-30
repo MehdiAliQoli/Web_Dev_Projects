9ad2838f5a90e73d93bcae606cb802162a671bb2
Mehdi Ali Qoli <2022n03925@gmail.com>
2026-05-30 20:46:47 +0500

    class and obj

---
+++ Class and Object/class_object.js
@@ -0,0 +1,10 @@
+class student{
+    constructor(name , rollNo , subject){
+        this.name = name;
+        this.rollNo = rollNo;
+        this.subject = subject;
+    }
+}
+
+const s1 = new student("Mehdi Ali" , 30507 , ["Bio", "Che", "Math"]);
+console.log();
