60c8c1b9d5a3fe5ab4d72538b73b27149ce94e17
Mehdi Ali Qoli <2022n03925@gmail.com>
2026-06-01 18:53:08 +0500

    added the react environment

---
+++ Basic to React/component.jsx
@@ -0,0 +1,8 @@
+
+const click_me = document.getElementById("Click_me")
+const dv = document.getElementById("screen")
+function myapp(){
+    const k = document.createAttribute("p");
+    p.innerHTML = "i am the magic idot";
+
+}
